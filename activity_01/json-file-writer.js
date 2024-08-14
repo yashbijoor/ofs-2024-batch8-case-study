b45033cce403ce1751b138c6127bd72547e4260a
yashbijoor <yashbijoor2002@gmail.com>
2024-08-14 14:34:17 +0530

    Activity_01 updated

--- activity_01/json-file-writer.js
+++ activity_01/json-file-writer.js
@@ -6,11 +6,13 @@ let employee1 = new Employee(1, "Jack", 60000);
 let employee2 = new Employee(2, "Mark", 65000);
 let employee3 = new Employee(3, "Mary", 70000);
 
-console.log(employee1);
 const jsonString = JSON.stringify(employee1);
-
-console.log(jsonString);
 
 fs.writeFileSync("employee.json", jsonString);
 
 console.log("JSON file created");
+
+let fileData = fs.readFileSync("employee.json");
+
+console.log("*************CONTENT OF THE JSON FILE**************");
+console.log(JSON.parse(fileData));
